970b5c98c9d4ab3062dd3c1f4fed59b4693d032b
Kai <kai@fam-pietruska.de>
2025-04-30 14:52:49 +0200

    updated beerbar tests to new setup
    .

--- tests/test_beerbar.py
+++ tests/test_beerbar.py
@@ -5,31 +5,28 @@ class TestClass:
     def test_require(self):
         ''' Test line 54 "require(hasRole)" in the BeerBar contract '''
         result: MapperResult = Mapper.map_hex_address(
-            combined_json_path="tests/compiler0826/compiled/BeerBar.json",
-            address_hex="773",
-            contract_name="BeerBar.sol",
-            contracts_folder="tests/compiler0826")
-        self._assert_code_in_line(result, 'require(hasRole(OWNER, msg.sender), "Barkeepers have to be set by owners")', 54)
+            compiler_output_json="tests/compiler0826/compiled/BeerBar.json",
+            address_hex="18AB",
+            contract_name="BeerBar",)
+        self._assert_code_in_line(result, 'hasRole(OWNER, msg.sender)', 54)
     
 
     def test_PUSH32(self):
         ''' Test line 56 "emit BarkeeperAdded(account)" in the BeerBar contract.
          This conains a PUSH32 instruction.'''
         result: MapperResult = Mapper.map_hex_address(
-            combined_json_path="tests/compiler0826/compiled/BeerBar.json",
-            address_hex="79F",
-            contract_name="BeerBar.sol",
-            contracts_folder="tests/compiler0826")
+            compiler_output_json="tests/compiler0826/compiled/BeerBar.json",
+            address_hex="193F",
+            contract_name="BeerBar")
         self._assert_code_in_line(result, 'BarkeeperAdded(account)', 56)
     
     def test_after_PUSH32(self):
         ''' Test line 56 "emit BarkeeperAdded(account)" in the BeerBar contract.
          This instruction is one after the PUSH32.'''
         result: MapperResult = Mapper.map_hex_address(
-            combined_json_path="tests/compiler0826/compiled/BeerBar.json",
-            address_hex="7C0",
-            contract_name="BeerBar.sol",
-            contracts_folder="tests/compiler0826")
+            compiler_output_json="tests/compiler0826/compiled/BeerBar.json",
+            address_hex="1940",
+            contract_name="BeerBar")
         self._assert_code_in_line(result, 'BarkeeperAdded(account)', 56)
 
 
@@ -37,5 +34,5 @@ class TestClass:
     def _assert_code_in_line(self, result: any, MapperResult: str, line: int):
         assert (result != None)
         assert (MapperResult == result.code)
-        assert (result.file == "tests\\compiler0826\\Contracts\\BeerBar.sol")
+        assert (result.file == "contracts/BeerBar.sol")
         assert (result.line == line)
