19db938ff4c9beb7fe76729891d0bd02c2e63c77
Gernot Salzer <salzer@logic.at>
2025-05-03 23:27:02 +0200

    more beerbar tests

---
+++ tests/test_beerbar.py
@@ -0,0 +1,30 @@
+from pytest_csv_params.decorator import csv_params
+
+import os
+(DIR,FILE) = os.path.split(__file__)
+(BASE,EXT) = os.path.splitext(FILE)
+
+from solidity_address_mapper.mapper import Mapper, MapperResult
+
+@csv_params(
+    base_dir=DIR,
+    data_file=f"{BASE}.csv",
+    id_col="id"
+)
+
+def test_mapper(
+        compiler_output_json: str,
+        address_hex: str,
+        contract_name: str,
+        filename: str,
+        source_code: str,
+        source_line: str,
+        info: str):
+    result: MapperResult = Mapper.map_hex_address(
+        compiler_output_json,
+        address_hex,
+        contract_name)
+    assert (result != None)
+    assert (result.file == filename)
+    assert (result.code == source_code.replace("\\r","\r").replace("\\n","\n"))
+    assert (result.line == int(source_line))
